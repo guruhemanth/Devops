bedad5c33be6569829e89a2c513ee63ad74d59ab
guruhemanth <guruhemanth0@gmail.com>
2026-07-01 14:37:38 +0530

    host added in server

--- azure/Training/hello.py
+++ azure/Training/hello.py
@@ -10,6 +10,6 @@ def hello():
 
 if __name__ == "__main__":
     # Run the server on port 8000
-    app.run(port=8000, debug=True)
+    app.run(port=8000,host="0.0.0.0", debug=True)
     
 print("helloe world")
